4b462596a20dd2a3236e82401dc46b6b3f9b8732
Neandher Carlos <neandher89@gmail.com>
2024-06-14 01:29:11 -0300

    logs

--- app/Http/Controllers/System/LogUsersAdController.php
+++ app/Http/Controllers/System/LogUsersAdController.php
@@ -4,16 +4,35 @@ namespace App\Http\Controllers\System;
 
 use App\Models\LogUsersAd;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Database\Eloquent\Builder;
 
 class LogUsersAdController extends Controller
 {
     public function getAll()
     {
-        $items = LogUsersAd::all();
+        $query = LogUsersAd::query()->where('id', '>', 0);
 
-        $data = ['data' => $items];
+        $qs = request()->all();
 
-        return response()->json($data);
+        if (isset($qs['search']) && !empty($qs['search'])) {
+            $query
+                ->where(
+                    function (Builder $query) use ($qs) {
+                        $query
+                            ->orWhere('nome', 'like',  '%' . $qs['search'] . '%')
+                            ->orWhere('cpf', '=',  $qs['search'])
+                            ->orWhere('matricula', '=',  $qs['search'])
+                            ->orWhere('login', 'like',  '%' . $qs['search'] . '%')
+                            ->orWhere('evento', '=',  $qs['search'])
+                            ->orWhere('status', '=',  $qs['search']);
+                    }
+                );
+        }
+
+        $items = $query->orderBy('created_at', 'desc')->paginate(30);
+
+        return response()->json($items);
     }
 }
